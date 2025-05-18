296eb57bbfbc1d53176b27dfdad5a8d5f5177c9a
Manuel <manuelcorreauy@gmail.com>
2025-05-18 16:52:09 -0300

    feature/PAI-5: Se creo la entidad usuario

---
+++ usuarioservice/src/main/java/com/presupuestos/usuarioservice/model/Usuario.java
@@ -0,0 +1,72 @@
+package com.presupuestos.usuarioservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    private boolean activo;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String email, Rol rol, boolean activo) {
+        this.nombre = nombre;
+        this.email = email;
+        this.rol = rol;
+        this.activo = activo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+}
