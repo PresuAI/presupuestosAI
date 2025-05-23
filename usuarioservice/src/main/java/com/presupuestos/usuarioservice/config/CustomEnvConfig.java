f19c71fc9faa080d0b1fb0c336a423f510c457d7
Manuel <manuelcorreauy@gmail.com>
2025-05-22 22:56:30 -0300

    Creacion de integration test para obtener usuarios

--- usuarioservice/src/main/java/com/presupuestos/usuarioservice/config/CustomEnvConfig.java
+++ usuarioservice/src/main/java/com/presupuestos/usuarioservice/config/CustomEnvConfig.java
@@ -3,8 +3,10 @@ package com.presupuestos.usuarioservice.config;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class CustomEnvConfig {
 
     @Bean
--- usuarioservice/src/test/java/com/presupuestos/usuarioservice/config/DotenvTestConfig.java
+++ usuarioservice/src/test/java/com/presupuestos/usuarioservice/config/DotenvTestConfig.java
@@ -1,10 +1,10 @@
 package com.presupuestos.usuarioservice.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @TestConfiguration
@@ -12,9 +12,11 @@ public class DotenvTestConfig {
 
     @Bean
     public Dotenv dotenv() {
-        Dotenv mockDotenv = Mockito.mock(Dotenv.class);
+        Dotenv mockDotenv = mock(Dotenv.class);
         when(mockDotenv.get("JWT_SECRET")).thenReturn("clave_super_segura_de_prueba");
-        when(mockDotenv.get("WHITELIST_EMAILS")).thenReturn("test@gmail.com");
+        when(mockDotenv.get("GOOGLE_CLIENT_ID")).thenReturn("test-client-id");
+        when(mockDotenv.get("GOOGLE_CLIENT_SECRET")).thenReturn("test-client-secret");
+        when(mockDotenv.get("GOOGLE_REDIRECT_URI")).thenReturn("http://localhost/test");
         return mockDotenv;
     }
 }
