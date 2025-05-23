5b11ee51a6e5a4637a211e29a8096221a35b1dfd
Manuel <manuelcorreauy@gmail.com>
2025-05-22 22:56:30 -0300

    Creacion de integration test para obtener usuarios

---
+++ usuarioservice/src/test/java/com/presupuestos/usuarioservice/integration/UsuarioIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.presupuestos.usuarioservice.integration;
+
+import com.presupuestos.usuarioservice.model.Rol;
+import com.presupuestos.usuarioservice.model.Usuario;
+import com.presupuestos.usuarioservice.repository.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsuarioIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeEach
+    void setUp() {
+        usuarioRepository.deleteAll();
+
+        Usuario user1 = new Usuario("Manuel", "manuel@gmail.com", Rol.ADMIN, true);
+        Usuario user2 = new Usuario("Santiago", "santiago@gmail.com", Rol.USUARIO, true);
+        usuarioRepository.saveAll(List.of(user1, user2));
+    }
+
+    @Test
+    @WithMockUser(username = "admin@gmail.com", roles = {"SUPERADMIN"})
+    void obtenerUsuarios_deberiaRetornar200YListaCorrecta() throws Exception {
+        mockMvc.perform(get("/api/usuarios")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(2)))
+                .andExpect(jsonPath("$[0].email", is("manuel@gmail.com")))
+                .andExpect(jsonPath("$[1].rol", is("USUARIO")));
+    }
+}
