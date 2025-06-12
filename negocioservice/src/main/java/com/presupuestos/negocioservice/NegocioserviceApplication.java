711233e56fb211caeb1ad495d23a381139b7410b
Manuel <manuelcorreauy@gmail.com>
2025-06-11 23:35:24 -0300

    PAI-36: Creacion de la estructura base del microservicio negocioservice

---
+++ negocioservice/src/main/java/com/presupuestos/negocioservice/NegocioserviceApplication.java
@@ -0,0 +1,13 @@
+package com.presupuestos.negocioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocioserviceApplication.class, args);
+	}
+
+}
